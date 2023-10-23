a14983cd6aedc33c698d8aea471f6170ab423cd7
Simanto1230 <140556982+Simanto1230@users.noreply.github.com>
2023-10-23 12:11:50 +0800

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+var btn= document.querySelectorAll('.tab-btn');
+var tab= document.querySelectorAll('.tab');
+
+
+function reset() {
+	for (var i = 0; i < tab.length; i++) 
+		{tab[i].style.display='none';
+		btn[i].style.backgroundColor='#222';}
+}
+
+function tabone(){
+	reset();
+	tab[0].style.display='block';
+	btn[0].style.backgroundColor='#08c'
+}tabone();
+
+for (let a=0; a< btn.length;a++){
+	btn[a].addEventListener('click',function (){
+		reset();
+		tab[a].style.display='block';
+		btn[a].style.backgroundColor= '#229dbc';
+
+	});
+}
+
